e1166b4cae721ba054fc5f68eddf6517f855d961
abisheek23 <abisheek2397@gmail.com>
2024-10-22 11:35:40 +0530

    todo

--- todo/src/Component/Tasklist.jsx
+++ todo/src/Component/Tasklist.jsx
@@ -10,7 +10,19 @@ const Tasklist =()=>{
         axios.get('https://aswanth74.pythonanywhere.com/api/tasks/')
         .then(response =>setTasks(response.data))
         .catch(error => console.log(error));
-    },[])
+    },[]);
+    const editTask=(task)=>{
+        setEditing(true);
+        setCurrentTask(task);
+    };
+    const updateTask=(id,updateTask)=>{
+        setEditing(false);
+        axios.put(`https://aswanth74.pythonanywhere.com/api/tasks/${id}/`,updateTask)
+        .then(response=>{
+            setTasks(tasks.map(task=>(task.id === id? response.data:task)));
+        })
+        .catch(error=>console.log(error));
+    }
     return(
         <div className='container mt-3'>
             <h2>Task List</h2>
@@ -27,11 +39,50 @@ const Tasklist =()=>{
            {editing ? (
             <EditTaskForm
             currentTask={currentTask}
-            updatetask={updateTask}
+            updateTask={updateTask}
             />
            ):null} 
 
         </div>
     );
 };
-export default Tasklist 
+const EditTaskForm=({currentTask,updateTask})=>{
+    const [task,setTask] =useState(currentTask);
+
+    const handleInputChange =(e)=>{
+        const{name,value}=e.target;
+        setTask({...task,[name]:value});
+    };
+    const handleSubmit =(e)=>{
+        e.preventDefault();
+        updateTask(task.id,task);
+    }
+    return(
+        <form  onSubmit={handleSubmit}>
+            <h2>Edit Task</h2>
+            <div>
+                <label >Title</label>
+                <input type="text"
+                name='title'
+                value={task.title} 
+                onChange={handleInputChange
+                }/>
+
+            </div>
+            <div>
+                <label>Discription</label>
+                <textarea name="description"
+                value={task.description}
+                onChange={handleInputChange}
+                />
+            </div>
+            <button type='submit'>updateTask Task</button>
+
+        </form>
+    )
+}
+
+export default Tasklist     
+
+
+
